93549dd7779bb503721fcd2bd06e66c0fbf030f2
Yasir OFLAZ <mustafayasirflz01@gmail.com>
2026-07-01 11:44:49 +0200

    Initial Commit

---
+++ SuperKahramanUI/SuperKahramanUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanUIApp.swift
+//  SuperKahramanUI
+//
+//  Created by Yasir OFLAZ on 1.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
